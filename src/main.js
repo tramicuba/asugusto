ebc1b944da903a787ff2382861468e0c35d13ead
tramicuba <artydj1997@gmail.com>
2026-09-16 08:38:50 -0400

    a
    a

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // Punto de entrada principal de AsuGusto
 
 import { initRouter } from "./router.js";
+import "../styles/global.css";
 
 // Selecciona el contenedor principal donde se renderizará la app
 const app = document.getElementById("app");
